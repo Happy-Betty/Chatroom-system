8dad5c88b853a31b2bec3c115aad85137b2a56c6
joelemenyu20 <emenyujoel2000@gmail.com>
2023-06-26 14:35:45 +0300

    changes

--- ChatMessage.java
+++ ChatMessage.java
@@ -4,7 +4,6 @@ import java.io.*;
  * Clients and the Server.
  * When talking from a Java Client to a Java Server a lot easier to pass Java objects, no
  * need to count bytes or to wait for a line feed at the end of the frame
- * author Rassul
  */
 
 public class ChatMessage implements Serializable {
